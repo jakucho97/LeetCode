b5fa80ece421e8108561029ab75d3e6349fae713
jakucho97 <jkchobot@gmail.com>
2024-09-27 17:57:43 +0200

    [182] [Time Beats: 90.10%] - LeetPush

---
+++ 182. Duplicate Emails/182-Duplicate-Emails.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+    return person[person.duplicated(subset=['email'])].rename(columns={'email':'Email'})[['Email']].drop_duplicates()
